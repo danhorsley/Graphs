30ceafdf734f351cf61549bb3975568e261da167
danielhorsley <danielhorsley@users.noreply.github.com>
2019-11-04 08:09:41 +0000

    tools update

--- projects/adventure/my_utils.py
+++ projects/adventure/my_utils.py
@@ -24,48 +24,102 @@ class Stack():
     def size(self):
         return len(self.stack)
 
-def traverse_cliques(cliques, parent_world, starting_point = 0):
-  traverses = []
-  for clique in cliques:
-    my_room_graph = {}
-    for item in clique:
-      my_room_graph[item] = parent_world.rg[item]
-    
-    for key in my_room_graph:
-      clean_dict = {}
-      directions = my_room_graph[key][1]
-      #print(directions)
-      for d in directions.keys():
-        if directions[d] in clique:
-          #print(directions[d])
-          clean_dict[d] = directions[d]
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+    def add_vertex(self, vertex):
+        """
+        Add a vertex to the graph.
+        """
+        if vertex not in self.vertices.keys():
+          self.vertices[vertex] = set()
         else:
           pass
-      my_room_graph[key] = [my_room_graph[key][0],clean_dict]
-    #print(my_room_graph)
-    clique_world = World()
-    clique_world.loadGraph(my_room_graph,start_room = min(list(my_room_graph.keys())))
-    #clique_world.printRooms
-    a,b = traverse(clique_world, starting_point = starting_point)
-    _ = [a,b,clique_world]
-    traverses.append(_)
-  for trav in traverses:
-    #print(trav[0][-1],trav[0][0])
-    _ = backtrack(trav[2],trav[0][-1],trav[0][0])
-    __ = dirs_for_path(_,trav[2])
-    trav[0] += _[1:-1]
-    trav[1] += __[1:-1]
-    
-  return traverses
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        if v1 and v2 in self.vertices.keys():
+         #self.vertices[v2].add(v1)
+          self.vertices[v1].add(v2)
 
-def backtrack(world, starting_vertex, destination_vertex):
-        """breadth firsf search to find shortest way"""
+    def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
+        if starting_vertex is None:
+          return None
+        my_q = Queue()
+        visited = [starting_vertex]
+        my_q.enqueue(starting_vertex)
+        while len(my_q.queue) > 0:
+          point = my_q.queue[0]
+          joins = self.vertices[point]
+          for j in joins:
+            if j not in visited:
+              my_q.enqueue(j)
+              visited.append(j)
+          print(my_q.dequeue())
+          
+
+
+    def dft(self, starting_vertex):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        """
+        ret_list = []
+        if starting_vertex is None:
+          return None
+        my_s = Stack()
+        visited = [starting_vertex]
+        my_s.push(starting_vertex)
+        while len(my_s.stack) > 0:
+          point = my_s.stack[-1]
+          joins = self.vertices[point]
+          r = my_s.pop()   ##new code
+          ret_list.append(r)  ##new code
+          #print(r)  ##changed to r from pop
+          for j in joins:
+            if j not in visited:
+              my_s.push(j)
+              visited.append(j)
+        return ret_list
+
+
+    def dft_recursive(self, starting_vertex, visited = []):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        This should be done using recursion.
+        """
+        print(starting_vertex)
+        visited.append(starting_vertex)
+        joins = self.vertices[starting_vertex]
+        if joins is None:
+          return None
+        for j in joins:
+          if j in visited:
+            pass
+          else:
+            self.dft_recursive(j,visited)
+
+
+
+    def bfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing the shortest path from
+        starting_vertex to destination_vertex in
+        breath-first order.
+        """
         q = Queue()
         q.enqueue([starting_vertex])
         
         while destination_vertex not in q.queue[0]:
           current_point = q.queue[0][-1]
-          joins = world.rg[current_point][1].values() #self.vertices[current_point]
+          joins = self.vertices[current_point]
           for j in joins:
             _ = [x for x in q.queue[0]]
             _.append(j)
@@ -73,6 +127,57 @@ def backtrack(world, starting_vertex, destination_vertex):
           q.dequeue()
 
         return q.queue[0]
+
+          
+
+
+    def dfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing a path from
+        starting_vertex to destination_vertex in
+        depth-first order.
+        """
+        s = Stack()
+        s.push([starting_vertex])
+        
+        while destination_vertex not in s.stack[-1]:
+          current_point = s.stack[-1][-1]
+          
+          joins = self.vertices[current_point]
+          if joins is None:
+            s.pop()
+          else:
+            temp_list = []
+            for j in joins:
+              _ = [x for x in s.stack[-1]]
+              _.append(j)
+              temp_list.append(_)
+            for tl in temp_list:
+              s.push(tl)
+          #s.pop()
+
+        return s.stack[-1]
+
+def roomgraph_to_graph(roomGraph):
+  my_graph = Graph()
+  connection_strip = {}
+  for key in roomGraph:
+    my_graph.add_vertex(key)
+    connection_strip[key] = set(roomGraph[key][1].values())
+  my_graph.vertices = connection_strip
+  return my_graph
+
+def connect_the_dots(path, graph):
+  n = len(path)
+  working_path = []
+  for i in range(n-1):
+    if path[i+1] in graph.vertices[path[i]]:
+      working_path.append(path[i])
+    else:
+      backtrack = graph.bfs(path[i],path[i+1])
+      for b in backtrack[:-1]:
+        working_path.append(b)
+  return working_path
 
 def dirs_for_path(my_list,world):
   point = my_list[0]
@@ -82,30 +187,3 @@ def dirs_for_path(my_list,world):
       if world.rg[point][1][d]==my_list[i]:
         dirs.append(d)
   return dirs
-
-def de_looper(trav, world, max_depth = 15 ):
-  better_list = [None] * len(trav)
-  index = 0
-  while index < len(trav):
-    point = trav[index]
-    print(' pt ',point, end='')
-    i = index + 1
-    while i < len(trav) and point != trav[i] and (i-index)<max_depth:
-      i +=1
-    if i  < len(trav):
-      my_slice = trav[index : i+1]
-      tt = traverse_cliques([set(my_slice)],world, starting_point = point)
-      if (len(tt[0][0]) + 1 )< i - index:
-        print('tt0',tt[0][0], 'i - index is', i - index)
-        better_list[index:index+len(tt[0][0])] = tt[0][0] + [point]
-        index = i
-      else:
-        better_list[index:index+i] = trav[index:index+i]
-        index +=1
-    elif (index == len(trav)-1) and (i==len(trav)):
-      better_list[index-1:] = trav[index-1:]
-      index +=1
-    else:
-      index +=1
-  better_list = [x for x in better_list if x is not None]
-  return better_list
